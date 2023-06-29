6182d3b103d697672e0c0712a09b9c05972761f8
RoninsWork <130744166+verygood-agency@users.noreply.github.com>
2023-06-29 13:00:27 +0300

    закончил адаптив

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 // техническая часть - УДАЛИТЬ НА ПРОДАКШЕНЕ!
 // получить в консоли элемент, по которому кликнули
 document.addEventListener('click', e => console.log(e.target));
+
+
+// выбираем все элементы с классом .custom-filter__link
+let filterLinks = document.querySelectorAll('.custom-filter__link');
+
+// перебираем все найденные элементы
+filterLinks.forEach(function(link) {
+    // добавляем слушатель события 'click' на каждый элемент
+    link.addEventListener('click', function(e) {
+        // предотвращаем стандартное действие ссылки
+        e.preventDefault();
+
+        // удаляем класс .active со всех элементов .custom-filter__link
+        filterLinks.forEach(function(innerLink) {
+            innerLink.classList.remove('active');
+        });
+
+        // добавляем класс .active тому элементу, по которому кликнули
+        this.classList.add('active');
+    });
+});
