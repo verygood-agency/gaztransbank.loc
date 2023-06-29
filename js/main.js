d1e2e50366c929311c06ba4c3e434dafc397a827
RoninsWork <130744166+verygood-agency@users.noreply.github.com>
2023-06-29 13:13:54 +0300

    Завершаю наполнение типовых страниц (картинки и заголовки)

--- js/main.js
+++ js/main.js
@@ -4,21 +4,24 @@ document.addEventListener('click', e => console.log(e.target));
 
 
 // выбираем все элементы с классом .custom-filter__link
-let filterLinks = document.querySelectorAll('.custom-filter__link');
+if (document.querySelector('.custom-filter__link')) {
+  let filterLinks = document.querySelectorAll('.custom-filter__link');
 
-// перебираем все найденные элементы
-filterLinks.forEach(function(link) {
+  // перебираем все найденные элементы
+  filterLinks.forEach(function (link) {
     // добавляем слушатель события 'click' на каждый элемент
-    link.addEventListener('click', function(e) {
-        // предотвращаем стандартное действие ссылки
-        e.preventDefault();
+    link.addEventListener('click', function (e) {
+      // предотвращаем стандартное действие ссылки
+      e.preventDefault();
 
-        // удаляем класс .active со всех элементов .custom-filter__link
-        filterLinks.forEach(function(innerLink) {
-            innerLink.classList.remove('active');
-        });
+      // удаляем класс .active со всех элементов .custom-filter__link
+      filterLinks.forEach(function (innerLink) {
+        innerLink.classList.remove('active');
+      });
 
-        // добавляем класс .active тому элементу, по которому кликнули
-        this.classList.add('active');
+      // добавляем класс .active тому элементу, по которому кликнули
+      this.classList.add('active');
     });
-});
+  });
+}
+
